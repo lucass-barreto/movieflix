6962afe532285c9186dfd0cbd9711cad209a5cfb
Lucas <lucasbo.dev@gmail.com>
2025-10-22 22:06:36 -0300

    feat(category): add category entity, repository, and migration script
    Introduced `Category` entity with JPA annotations and a corresponding repository for database interaction. Added an initial SQL migration script to create the `category_tb` table.

---
+++ src/main/java/br/com/movieflix/entity/Category.java
@@ -0,0 +1,31 @@
+package br.com.movieflix.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category_tb")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
