d5ce0be4aedc72b101edba68812793ec5811160d
Lucas <lucasbo.dev@gmail.com>
2025-10-30 16:22:19 -0300

    feat(mapper): add movie mapper for request and response conversion
    Introduced `MovieMapper` utility class to handle transformations between `MovieRequest`, `MovieResponse`, and `Movie` entities. Simplified data handling by providing static methods for mapping categories and streamings within movies. Added corresponding `MovieRequest` and `MovieResponse` classes to define request and response objects.

---
+++ src/main/java/br/com/movieflix/controller/response/MovieResponse.java
@@ -0,0 +1,15 @@
+package br.com.movieflix.controller.response;
+
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record MovieResponse(String title,
+                            String description,
+                            LocalDate releaseDate,
+                            double rating,
+                            List<CategoryResponse> categories,
+                            List<StreamingResponse> streamings) {
+}
---
+++ src/main/java/br/com/movieflix/mapper/MovieMapper.java
@@ -0,0 +1,58 @@
+package br.com.movieflix.mapper;
+
+import br.com.movieflix.controller.request.MovieRequest;
+import br.com.movieflix.controller.response.CategoryResponse;
+import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.controller.response.StreamingResponse;
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+
+@UtilityClass
+public class MovieMapper {
+
+    private CategoryMapper categoryMapper;
+
+    public static Movie toModel(MovieRequest request) {
+
+        List<Category> categories = request.categories().stream()
+                .map(categoryId -> Category.builder().id(categoryId).build())
+                .toList();
+
+        List<Streaming> streamings = request.streamings().stream()
+                .map(streamingsId -> Streaming.builder().id(streamingsId).build())
+                .toList();
+
+        return Movie.builder()
+                .title(request.title())
+                .description(request.description())
+                .releaseDate(request.releaseDate())
+                .rating(request.rating())
+                .categories(categories)
+                .streamings(streamings)
+                .build();
+    }
+
+    public static MovieResponse toMovieResponse(Movie movie) {
+
+        List<CategoryResponse> categories = movie.getCategories().stream().
+                map(CategoryMapper::toCategoryResponse)
+                .toList();
+
+        List<StreamingResponse> streamings = movie.getStreamings().stream()
+                .map(StreamingMapper::toStreamingResponse)
+                .toList();
+
+        return MovieResponse.builder()
+                .title(movie.getTitle())
+                .description(movie.getDescription())
+                .releaseDate(movie.getReleaseDate())
+                .rating(movie.getRating())
+                .categories(categories)
+                .streamings(streamings)
+                .build();
+    }
+}
